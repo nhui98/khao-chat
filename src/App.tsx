f92f2b3bfdb5b52f88961cbd459366b0cd57336e
Nathan <hui.n1998@gmail.com>
2022-09-09 15:40:52 +0100

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const App = () => <Register />;
-
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default App;
