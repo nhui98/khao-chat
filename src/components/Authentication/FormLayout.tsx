e77764f9b60073a6b4a3d900a9859acdadd90dbb
Nathan <hui.n1998@gmail.com>
2022-09-09 19:37:25 +0100

    form layout classes reorder

--- src/components/Authentication/FormLayout.tsx
+++ src/components/Authentication/FormLayout.tsx
@@ -5,10 +5,10 @@ type FormLayoutProps = {
 
 const FormLayout = ({ children, title }: FormLayoutProps) => {
   return (
-    <div className="bg-[#a7bcff] min-h-screen flex items-center justify-center">
-      <div className="bg-white py-5 px-16 flex flex-col items-center rounded-lg gap-y-3">
-        <span className="text-[#5d5b8d] text-2xl font-bold">Khao Chat</span>
-        <h2 className="text-[#5d5b8d] text-base">{title}</h2>
+    <div className="flex min-h-screen items-center justify-center bg-[#a7bcff]">
+      <div className="flex flex-col items-center gap-y-3 rounded-lg bg-white py-5 px-16">
+        <span className="text-2xl font-bold text-[#5d5b8d]">Khao Chat</span>
+        <h2 className="text-base text-[#5d5b8d]">{title}</h2>
         {children}
       </div>
     </div>
